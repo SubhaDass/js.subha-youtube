a089db037313c82fb15ffaf28c2ed8c12ce2c2a0
SubhaDass <thesubhadas899@gmail.com>
2025-10-04 19:58:31 +0000

    datatypes

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,17 @@
+
+let age = 23;
+let name = "subha";
+let isLogged = true;
+let state = null;
+
+// number -> 2 to the power 53
+// bigint
+// string -> ""
+// boolen -> true/false
+// null -> standalone value
+// undefined -> 
+// symbol -> unique
+
+// objects
+
+console.log(typeof 10);
