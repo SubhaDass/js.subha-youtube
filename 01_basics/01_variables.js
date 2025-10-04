bcef9d6512656dd17bd6c9cabf3fa358a1bee2a7
SubhaDass <thesubhadas899@gmail.com>
2025-10-04 19:58:11 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const pi = 3.14;
+let city = "kolkata";
+var name = "subha"
+
+console.table([pi, city, name]);
+
+city = "delhi";
+
+console.table([pi, city, name]);
+
+// const is a constant variable which is unchangable
+// let is chagable and massively use 
+// var is outdated its not recognize scopes
