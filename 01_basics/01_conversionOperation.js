58be7c6ea0620571563ac2ee2d08d02e9e36ae40
SubhaDass <thesubhadas899@gmail.com>
2025-10-05 18:51:57 +0000

    convertion of datatypes

---
+++ 01_basics/01_conversionOperation.js
@@ -0,0 +1,30 @@
+let score = "subhaaa"
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueInNumber = Number(score);
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+// "33" -> 33
+// "33abc" -> NaN
+// true -> 1 , false -> 0
+
+let isLoggedIn = "hjjc"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(typeof booleanIsLoggedIn);
+//console.log(booleanIsLoggedIn);
+
+// 1 -> true, 0 -> false
+// "" -> false
+// "kvssd" -> true
+
+let someNumber = 45
+console.log(typeof someNumber);
+
+let stringnumber = String(someNumber)
+console.log(stringnumber);
+console.log(typeof stringnumber);
+
