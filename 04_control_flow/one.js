96db28a448b91a7778242589c969c738e82cea4a
SubhaDass <thesubhadas899@gmail.com>
2025-10-09 11:09:03 +0000

    conditions (if, else)

---
+++ 04_control_flow/one.js
@@ -0,0 +1,14 @@
+// <, >, <=, >=, ==, ===, !=, !===
+
+const temparature = 50
+const currTemparature = 40
+
+if(temparature > currTemparature){
+    console.log("current temparature is low");
+}
+else {
+    console.log("current temparature is high");
+}
+console.log("weather")
+
+// && ad || operations 
