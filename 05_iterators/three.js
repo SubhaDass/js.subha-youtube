2633e6c3635333b16b751e27f7a4902c5000044f
SubhaDass <thesubhadas899@gmail.com>
2025-10-09 20:58:33 +0000

    for of loop

---
+++ 05_iterators/three.js
@@ -0,0 +1,34 @@
+// for of
+
+// 'for of' on array
+let arr = [2, 5 , 8, 4, 1]
+for (const num of arr) {
+    // console.log(`number is ${num}`);
+}
+
+// 'for of' on strings
+let wish = "happy birthday"
+for(const i of wish) {
+    // console.log(`wish you ${i}`);
+}
+
+// Maps
+const map = new Map()
+map.set("IN" , "INDIA")
+map.set("SL" , "SRILANKA")
+map.set("ENG" , "ENGLAND")
+// console.log(map);
+
+// 'for of' on Maps
+for(const [key, val] of map){
+    console.log(key, ":-", val);
+}
+
+// 'for of' on Objects
+const myObj = {
+    game1: "pes",
+    game2: "bgmi"
+}
+// for (const [key, val] of myObj) {
+//     console.log(key, ":-", val);
+// } ERROR
